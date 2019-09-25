d1b95fd1b5479af2b5dc02b19338e338620621b0
Ivan Grushevich <githubemail911@gmail.com>
2019-09-26 01:08:21 +0300

    Rename Aux component (for windows), add message to Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
-    return [...Array(props.ingredients[ingKey])].map((_, index) => (
-      <BurgerIngredient key={ingKey + index} type={ingKey} />
-    ));
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, index) => (
+        <BurgerIngredient key={ingKey + index} type={ingKey} />
+      ));
+    })
+    .reduce((arr, el) => (arr = [...arr, ...el]));
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
 
   return (
     <div className={classes.Burger}>
